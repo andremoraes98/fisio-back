280a1f7ac79ed3e89589dbf5abb03ba23eeb8af0
Andre Moraes <andre.moraes.98@gmail.com>
2023-02-09 21:13:10 -0300

    feat: criação do middleware para a rota login

---
+++ src/middleware/User.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from "express";
+import errors from 'restify-errors';
+import UserModel from "../model/User";
+import bcrypt from 'bcrypt';
+
+const userModel = new UserModel();
+
+const validateBody = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    throw new errors.MissingParameterError('Todos os campos são obrigatórios!');
+  }
+
+  next();
+};
+
+const validateEmail = async (req: Request, res: Response, next: NextFunction) => {
+  const { email } = req.body;
+
+  const user = await userModel.findOneWhereEmail(email);
+
+  if (!user) {
+    throw new errors.NotFoundError('Email não cadastrado.');
+  }
+
+  if (user.email !== email) {
+    throw new errors.NotAuthorizedError('Email incorreto');
+  }
+
+  next();
+};
+
+const validatePassword = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  const result = await userModel.findOneWhereEmail(email);
+
+  if (!result) {
+    throw new errors.NotFoundError('Email não cadastrado!');
+  }
+
+  if (!bcrypt.compareSync(password, result.password)) {
+    throw new errors.NotAuthorizedError('Senha incorreta!');
+  }
+
+  next();
+};
+
+export {
+  validateBody,
+  validateEmail,
+  validatePassword,
+}
