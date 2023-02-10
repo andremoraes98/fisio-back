cab05478e536ffe0697f9e376f9457eefa613ff1
Andre Moraes <andre.moraes.98@gmail.com>
2023-02-09 21:13:30 -0300

    feat: exposição da rota login

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from '../controler/User';
 import UserService from '../service/User';
 import UserModel from '../model/User';
+import { validateBody, validateEmail, validatePassword } from '../middleware/User';
 
 const route = Router();
 
@@ -14,5 +15,6 @@ route.get('/user/:id', (req, res) => userController.readOne(req, res));
 route.put('/user', (req, res) => userController.create(req, res));
 route.post('/user/:id', (req, res) => userController.updateOne(req, res));
 route.delete('/user/:id', (req, res) => userController.destroy(req, res));
+route.post('/login', validateBody, validateEmail, validatePassword, (req, res) => userController.login(req, res));
 
 export default route;
